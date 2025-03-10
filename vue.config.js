d1173d2d8637badf2f7dc15ae48385b633398c4a
Nuri12c <nurislam.ganieiev@mail.ru>
2025-03-10 16:50:47 +0300

    repo поменял

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+      publicPath: process.env.NODE_ENV === 'production'
+        ? '/public/' // Замените `<REPO>` на имя вашего репозитория
+        : '/'
 })
